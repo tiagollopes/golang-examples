e65a890829a8700025688f975d6352aebac8e92e
testejr <dfmlumbavygawniyew@nespj.com>
2026-01-15 18:45:18 -0300

    Adiciona exemplos básicos de main e variáveis

---
+++ variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Tipos de variáveis básicas
+	var inteiro int = 10
+	var decimal float64 = 3.14
+	var texto string = "Olá, Go!"
+	var booleano bool = true
+
+	fmt.Println("Inteiro: ", inteiro)
+	fmt.Println("Decimal: ", decimal)
+	fmt.Println("Texto : ", texto)
+	fmt.Println("Booleano : ", booleano)
+}
